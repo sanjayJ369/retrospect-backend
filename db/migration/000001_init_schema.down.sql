e2327e5152a155290eb037ef7f4d3a25ec3d98d9
sanjayj369 <j.sanjay336699@gmail.com>
2025-07-16 06:48:48 +0000

    update down migration schema

--- db/migration/000001_init_schema.down.sql
+++ db/migration/000001_init_schema.down.sql
@@ -1,5 +1,11 @@
+DROP VIEW IF EXISTS current_challenges_view;
+
 DROP TABLE IF EXISTS "tasks";
+
 DROP TABLE IF EXISTS "challenge_entries";
+
 DROP TABLE IF EXISTS "task_days";
+
 DROP TABLE IF EXISTS "challenges";
+
 DROP TABLE IF EXISTS "users";
