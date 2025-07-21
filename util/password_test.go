dba67938d0c2ca01266e009f6bd411c9b94b093b
sanjayj369 <j.sanjay336699@gmail.com>
2025-07-21 06:23:18 +0530

    Add password hashing and verification functions with tests

---
+++ util/password_test.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPassword(t *testing.T) {
+
+	password := GetRandomString(6)
+	hashPassword, err := HashedPassword(password)
+	require.NoError(t, err)
+
+	err = CheckPassword(password, hashPassword)
+	require.NoError(t, err)
+
+	wrongPassword := GetRandomString(6)
+	err = CheckPassword(wrongPassword, hashPassword)
+	require.NoError(t, err)
+}
